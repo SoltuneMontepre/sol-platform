bfc9c48bf51cb29c9fa8b1977fc972e665a60f4c
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2026-04-12 23:58:33 +0700

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { RouterProvider } from "react-router";
+import router from "./config/routes";
+
+const App = (): React.ReactElement => {
+  const queryClient = new QueryClient();
+  return (
+
+  );
+};
+
+export default App;
